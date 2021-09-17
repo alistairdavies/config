4e7281e41572a7a21aacc54856e9c5ab0b298068
Alistair Davies <Alistair.davies@arm.com>
2021-09-17 17:27:00 +0100

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,26 +1,3 @@
-local Plug = vim.fn['plug#']
-
-
-vim.call('plug#begin', '~/.config/nvim/plugged')
-
-Plug 'neovim/nvim-lspconfig'
-Plug 'nvim-treesitter/nvim-treesitter'
-Plug 'nvim-lua/plenary.nvim'
-Plug 'nvim-lua/popup.nvm'
-Plug 'nvim-telescope/telescope.nvim'
-Plug 'Vimjas/vim-python-pep8-indent'
-Plug 'janko-m/vim-test'
-
-vim.call('plug#end')
-
-
-vim.api.nvim_set_keymap('n', '<Leader>ff', ':Telescope find_files<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<Leader>fg', ':Telescope live_grep<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<Leader>fb', ':Telescope buffers<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<Leader>fh', ':Telescope help_tags<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<Leader>t', ':TestFile<CR>', { noremap = true, silent = true })
-
-
 vim.opt.ruler = true
 vim.opt.number = true
 vim.opt.showcmd = true
@@ -29,9 +6,40 @@ vim.opt.hlsearch = true
 vim.opt.mouse = "a"
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
+vim.opt.clipboard = "unnamed"
+vim.opt.termguicolors = true
+
+vim.opt.wildignore = {"*/node_modules/*", "*/static/*", "*/tmp/*"}
+
+local Plug = vim.fn['plug#']
 
 
-vim.api.nvim_command("colorscheme industry")
+vim.call('plug#begin', '~/.config/nvim/plugged')
+
+Plug 'neovim/nvim-lspconfig'
+Plug 'nvim-treesitter/nvim-treesitter'
+Plug 'nvim-lua/plenary.nvim'
+Plug 'nvim-telescope/telescope.nvim'
+Plug 'Vimjas/vim-python-pep8-indent'
+Plug 'janko-m/vim-test'
+Plug 'muchzill4/doubletrouble'
+
+vim.call('plug#end')
+
+
+vim.api.nvim_set_keymap('n', '<Leader>ff', ':Telescope find_files<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<Leader>fg', ':Telescope live_grep<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<Leader>fb', ':Telescope buffers<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<Leader>fd', ':Telescope file_browser<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<Leader>fh', ':Telescope help_tags<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<Leader>t', ':TestFile<CR>', { noremap = true, silent = true })
+
+
+vim.api.nvim_command("colorscheme doubletrouble")
 
 -- janko-m/vimtest
 vim.g['test#python#runner'] = 'pytest'
+
+local ok, lspconfig = pcall(require, "lspconfig")
+lspconfig.jedi_language_server.setup {
+}
