58c8896151584f53b7c5ed57fc5ebaf6d171249c
Alistair Davies <Alistair.davies@arm.com>
2022-05-10 15:50:44 +0100

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,3 @@
-set -x GOPATH $HOME/go
-
 set -g DOCKER_BUILDKIT 1
 set -g COMPOSE_DOCKER_CLI_BUILD 1
 status --is-interactive; and source (pyenv init -|psub)
@@ -14,11 +12,3 @@ end
 
 set -x EDITOR nvim
 set -x VISUAL nvim
-
-# Created by `pipx` on 2021-09-13 09:39:19
-set PATH $PATH /Users/alidav01/.local/bin
-
-# Created by `pipx` on 2021-09-13 09:39:23
-set PATH $PATH /Users/alidav01/Library/Python/3.9/bin
-fish_add_path /usr/local/opt/llvm/bin
-fish_add_path //Users/alidav01/.cargo/bin
