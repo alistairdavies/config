921d5b6f9d0e1d7aec1826a4a5d652c77090d878
Alistair Davies <alistair.davies@arm.com>
2022-07-13 10:54:53 +0100

    Add treesitter textobjects and context config

--- nvim/init.lua
+++ nvim/init.lua
@@ -12,6 +12,8 @@ vim.opt.termguicolors = true
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 
+vim.g.mapleader = ","
+
 vim.opt.wildignore = {"*/node_modules/*", "*/static/*", "*/tmp/*", "*/.mypy_cache/*"}
 
 local Plug = vim.fn['plug#']
@@ -21,6 +23,8 @@ vim.call('plug#begin', '~/.config/nvim/plugged')
 
 Plug 'neovim/nvim-lspconfig'
 Plug 'nvim-treesitter/nvim-treesitter'
+Plug 'nvim-treesitter/nvim-treesitter-context'
+Plug 'nvim-treesitter/nvim-treesitter-textobjects'
 Plug 'nvim-lua/plenary.nvim'
 Plug 'nvim-telescope/telescope.nvim'
 Plug 'nvim-telescope/telescope-file-browser.nvim'
@@ -39,30 +43,42 @@ Plug 'kyazdani42/nvim-web-devicons'
 vim.call('plug#end')
 
 
-
 local opts = {noremap=true, silent=true}
 
-vim.api.nvim_set_keymap('n', '<Leader>ff', ':Telescope find_files<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<Leader>fg', ':Telescope live_grep<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<Leader>fb', ':Telescope buffers<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<Leader>fd', ':Telescope file_browser<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<Leader>fh', ':Telescope help_tags<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<Leader>p', ':Telescope yacp<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<Leader>tt', ':TestFile<CR>', opts)
-vim.api.nvim_set_keymap('n', '<Leader>ts', ':TestSuite<CR>', opts)
-vim.api.nvim_set_keymap('n', '<Leader>m', ':lua vim.lsp.buf.formatting()<CR>")', opts)
-vim.api.nvim_set_keymap('n', '<Leader>l', ':TroubleToggle<CR>")', opts)
+vim.keymap.set('n', '<Leader>ff', ':Telescope find_files<CR>', { noremap = true })
+vim.keymap.set('n', '<Leader>fg', ':Telescope live_grep<CR>', { noremap = true })
+vim.keymap.set('n', '<Leader>fb', ':Telescope buffers<CR>', { noremap = true })
+vim.keymap.set('n', '<Leader>fd', ':Telescope file_browser<CR>', { noremap = true })
+vim.keymap.set('n', '<Leader>fh', ':Telescope help_tags<CR>', { noremap = true })
+vim.keymap.set('n', '<Leader>pp', ':Telescope yacp<CR>', { noremap = true })
+vim.keymap.set('n', '<Leader>pr', ':Telescope yacp replay<CR>', { noremap = true })
+vim.keymap.set('n', '<Leader>tt', ':TestFile<CR>', opts)
+vim.keymap.set('n', '<Leader>ts', ':TestSuite<CR>', opts)
+vim.keymap.set('n', '<Leader>m', ':lua vim.lsp.buf.formatting()<CR>")', opts)
+vim.keymap.set('n', '<Leader>we', ':TroubleToggle<CR>', opts)
+vim.keymap.set('n', '<Leader>ww', ':TroubleToggle workspace_diagnostics<CR>', opts)
+vim.keymap.set('n', '<Leader>o', ':Project<space>', { noremap = true})
+vim.keymap.set('n', '<Leader>s', ':w<CR>', { noremap = true })
+vim.keymap.set('n', ',', '\\', { noremap = true })
+vim.keymap.set('n', '\\', ',', { noremap = true })
 
-vim.api.nvim_set_keymap('n', '<Leader>ct', ':cargo test<CR>")', opts)
-vim.api.nvim_set_keymap('n', '<Leader>cc', ':cargo check<CR>")', opts)
 
 local on_attach = function(client, bufnr)
-  vim.api.nvim_set_keymap('n', '<Leader>gd', ':lua vim.lsp.buf.definition()<CR>', opts)
-  vim.api.nvim_set_keymap('n', '<Leader>gh', ':lua vim.lsp.buf.hover()<CR>', opts)
-  vim.api.nvim_set_keymap('n', '<Leader><C-k>', ':lua vim.lsp.buf.signature_help()<CR>', opts)
+  vim.keymap.set('n', '<Leader>gd', ':lua vim.lsp.buf.definition()<CR>', opts)
+  vim.keymap.set('n', '<Leader>gh', ':lua vim.lsp.buf.hover()<CR>', opts)
+  vim.keymap.set('n', '<Leader>gg', ':TroubleToggle lsp_references<CR>")', opts)
 end
 
 vim.api.nvim_command("colorscheme doubletrouble")
+
+vim.api.nvim_create_user_command(
+    'Project',
+    function(opts)
+      vim.api.nvim_command(string.format("lcd `z -e %s`", opts.args))
+    end, 
+    { nargs = 1, complete="dir"}
+)
+
 
 -- janko-m/vimtest
 vim.g['test#python#runner'] = 'pytest'
@@ -71,20 +87,20 @@ vim.g['test#python#runner'] = 'pytest'
 local telescope = require("telescope")
 telescope.load_extension "file_browser"
 
+
 -- Command Line Palette
 telescope.setup {
   extensions = {
     yacp = {
       palette = {
          { name = "pre-commit", cmd = "sp | term pre-commit run --all-files"},
+         { name = "open files with conflicts", cmd = "args `git diff --name-only --diff-filter=U`"},
          { name = "clear buffers", cmd = ":%bd|edit#|bd#"},
          { name = "new tab", cmd = ":tabnew"},
-
       }
     }
   }
 }
-
 telescope.load_extension "yacp"
 
 
@@ -95,7 +111,47 @@ capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 
--- Rust. brew install rust_analyzer
+-- Treesitter
+require('nvim-treesitter.configs').setup {
+  ensure_installed = { 'typescript', 'rust', 'python', 'yaml', 'markdown' },
+  highlight = { enable = true },
+  indent = { enable = true },
+  textobjects = {
+    select = {
+      enable = true,
+      lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+      keymaps = {
+        -- You can use the capture groups defined in textobjects.scm
+        ['af'] = '@function.outer',
+        ['if'] = '@function.inner',
+        ['ac'] = '@class.outer',
+        ['ic'] = '@class.inner',
+      },
+    },
+    swap = {
+      enable = true,
+      swap_next = {
+        ['<leader>a'] = '@parameter.inner',
+      },
+      swap_previous = {
+        ['<leader>A'] = '@parameter.inner',
+      },
+    },
+  },
+}
+
+
+local context = require "treesitter-context"
+if context then
+  context.setup {
+    max_lines = 1,
+    trim_scope = "inner",
+    mode = "topline",
+  }
+end
+
+
+-- Rust. brew install rust-analyzer
 lspconfig.rust_analyzer.setup{}
 
 -- C and C++ lsp. brew install llvm
